4e8343c321c40798c8c10d2e7571cdcf8200507e
ryym <ryym.64@gmail.com>
2018-01-08 23:07:45 +0900

    Copy table by reflection (for now)

--- query/gnr2/tables.go
+++ query/gnr2/tables.go
@@ -1,5 +1,7 @@
 package gnr2
 
+import "reflect"
+
 // XXX: 面倒だけど、テーブルは個別に定義するしかなさそう。
 
 type Joinner struct {
@@ -9,6 +11,26 @@ type Joinner struct {
 
 func (jd *Joinner) Inner() JoinOn {
 	return JoinOn{jd.table, jd.on, JOIN_INNER}
+}
+
+func copyTableAs(alias string, src Table, dest Table) {
+	srcV := reflect.ValueOf(src).Elem()
+	srcT := srcV.Type()
+	destV := reflect.ValueOf(dest).Elem()
+	for i := 0; i < srcT.NumField(); i++ {
+		f := srcT.Field(i)
+		if f.Type.Name() == "Column" {
+			orig := srcV.Field(i).Interface().(Column)
+			copy := column{
+				tableAlias: alias,
+				tableName:  orig.TableName(),
+				structName: orig.StructName(),
+				name:       orig.ColumnName(),
+				fieldName:  orig.FieldName(),
+			}.init()
+			destV.Field(i).Set(reflect.ValueOf(&copy))
+		}
+	}
 }
 
 type UsersTable struct {
@@ -43,24 +65,10 @@ func (t *UsersTable) Columns() []Column {
 
 // ちょっとコスト高すぎ..?
 func (t *UsersTable) As(alias string) *UsersTable {
-	origCols := t.Columns()
-	cols := make([]Column, len(origCols))
-	for i, c := range origCols {
-		col := column{
-			tableAlias: alias,
-			tableName:  c.TableName(),
-			structName: c.StructName(),
-			name:       c.ColumnName(),
-			fieldName:  c.FieldName(),
-		}.init()
-		cols[i] = &col
-	}
-
 	t2 := *t
 	t2.alias = alias
-	t2.ID = cols[0]
-	t2.Name = cols[1]
-	t2.SliceCollectorMaker = NewSliceCollectorMaker(t.empModel, cols, alias)
+	t2.SliceCollectorMaker = NewSliceCollectorMaker(t.empModel, t2.Columns(), alias)
+	copyTableAs(alias, t, &t2)
 	return &t2
 }
 
