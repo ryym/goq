339ec747850bfcc32a10ce6c36d8a0ab4ca57bec
ryym <ryym.64@gmail.com>
2018-01-14 21:21:15 +0900

    test(cllct): Add RowMapSliceCollector test

---
+++ cllct/row_map_slice_test.go
@@ -0,0 +1,41 @@
+package cllct
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/go-test/deep"
+	"github.com/ryym/goq/gql"
+)
+
+func TestRowMapSliceCollector(t *testing.T) {
+	var got []map[string]interface{}
+	cl := &RowMapSliceCollector{slice: &got}
+
+	rows := [][]interface{}{
+		{1, "foo", true},
+		{2, "bar", false},
+	}
+
+	cl.Init(
+		make([]gql.Selection, 3),
+		[]string{"a", "b", "c"},
+	)
+
+	for _, row := range rows {
+		ptrs := make([]interface{}, 3)
+		cl.Next(ptrs)
+		for i, p := range ptrs {
+			reflect.ValueOf(p).Elem().Set(reflect.ValueOf(row[i]))
+		}
+		cl.AfterScan(ptrs)
+	}
+
+	want := []map[string]interface{}{
+		{"a": 1, "b": "foo", "c": true},
+		{"a": 2, "b": "bar", "c": false},
+	}
+	if diff := deep.Equal(got, want); diff != nil {
+		t.Error(diff)
+	}
+}
