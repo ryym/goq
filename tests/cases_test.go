6f61c7677c3d3dcd63390800a6cb935735987053
ryym <ryym.64@gmail.com>
2018-02-03 12:02:50 +0900

    test(e2e): Test for collecting into slices and maps

--- tests/cases_test.go
+++ tests/cases_test.go
@@ -9,6 +9,8 @@ import (
 	"github.com/ryym/goq"
 )
 
+var defaultUpdatedAt = time.Date(2000, time.January, 1, 9, 0, 0, 0, time.UTC)
+
 var testCases = []testCase{
 	{
 		name: "select first one from multiple records",
@@ -217,6 +219,194 @@ var testCases = []testCase{
 			if diff := deep.Equal(cities, wantCities); diff != nil {
 				t.Log(q.Construct())
 				return fmt.Errorf("cities diff: %s", diff)
+			}
+
+			return nil
+		},
+	},
+	{
+		name: "select records by slice and map collectors",
+		data: `
+			INSERT INTO countries (id, name) VALUES (1, 'Japan');
+
+			INSERT INTO cities (country_id, id, name) VALUES (1, 11, 'tokyo');
+			INSERT INTO cities (country_id, id, name) VALUES (1, 12, 'hokkaido');
+			INSERT INTO cities (country_id, id, name) VALUES (1, 13, 'okinawa');
+
+			INSERT INTO addresses (city_id, id, name) VALUES (11, 111, 'shinjuku');
+			INSERT INTO addresses (city_id, id, name) VALUES (11, 112, 'yoyogi');
+			INSERT INTO addresses (city_id, id, name) VALUES (11, 113, 'nakano');
+			INSERT INTO addresses (city_id, id, name) VALUES (12, 121, 'sapporo');
+			INSERT INTO addresses (city_id, id, name) VALUES (12, 122, 'kushiro');
+			INSERT INTO addresses (city_id, id, name) VALUES (13, 131, 'naha');
+			INSERT INTO addresses (city_id, id, name) VALUES (13, 132, 'miyako');
+
+			INSERT INTO countries (id, name) VALUES (2, 'Somewhere');
+			INSERT INTO cities (country_id, id, name) VALUES (2, 21, 'city1');
+			INSERT INTO addresses (city_id, id, name) VALUES (21, 211, 'address1');
+			INSERT INTO addresses (city_id, id, name) VALUES (21, 212, 'address2');
+		`,
+		run: func(t *testing.T, tx *goq.Tx, z *Builder) error {
+			q := z.Select(
+				z.Countries.All(),
+				z.Cities.All(),
+				z.Addresses.All(),
+			).From(
+				z.Countries,
+			).Joins(
+				z.InnerJoin(z.Cities).On(
+					z.Cities.CountryID.Eq(z.Countries.ID),
+				),
+				z.InnerJoin(z.Addresses).On(
+					z.Addresses.CityID.Eq(z.Cities.ID),
+				),
+			).OrderBy(
+				z.Countries.ID,
+				z.Cities.ID,
+				z.Addresses.ID,
+			)
+
+			var countries []Country
+			var cities map[int][]City
+			var addresses map[int][]Address
+			err := tx.Query(q).Collect(
+				z.Countries.ToUniqSlice(&countries),
+				z.Cities.ToUniqSliceMap(&cities).By(z.Countries.ID),
+				z.Addresses.ToSliceMap(&addresses).By(z.Cities.ID),
+			)
+			if err != nil {
+				return err
+			}
+
+			wantCountries := []Country{
+				{1, "Japan", defaultUpdatedAt},
+				{2, "Somewhere", defaultUpdatedAt},
+			}
+			if diff := deep.Equal(countries, wantCountries); diff != nil {
+				t.Log(q.Construct())
+				return fmt.Errorf("countries diff: %s", diff)
+			}
+
+			wantCities := map[int][]City{
+				1: []City{
+					{11, "tokyo", 1, defaultUpdatedAt},
+					{12, "hokkaido", 1, defaultUpdatedAt},
+					{13, "okinawa", 1, defaultUpdatedAt},
+				},
+				2: []City{
+					{21, "city1", 2, defaultUpdatedAt},
+				},
+			}
+			if diff := deep.Equal(cities, wantCities); diff != nil {
+				t.Log(q.Construct())
+				return fmt.Errorf("cities diff: %s", diff)
+			}
+
+			wantAddresses := map[int][]Address{
+				11: []Address{
+					{111, "shinjuku", 11, defaultUpdatedAt},
+					{112, "yoyogi", 11, defaultUpdatedAt},
+					{113, "nakano", 11, defaultUpdatedAt},
+				},
+				12: []Address{
+					{121, "sapporo", 12, defaultUpdatedAt},
+					{122, "kushiro", 12, defaultUpdatedAt},
+				},
+				13: []Address{
+					{131, "naha", 13, defaultUpdatedAt},
+					{132, "miyako", 13, defaultUpdatedAt},
+				},
+				21: []Address{
+					{211, "address1", 21, defaultUpdatedAt},
+					{212, "address2", 21, defaultUpdatedAt},
+				},
+			}
+			if diff := deep.Equal(addresses, wantAddresses); diff != nil {
+				t.Log(q.Construct())
+				return fmt.Errorf("addresses diff: %s", diff)
+			}
+
+			return nil
+		},
+	},
+	{
+		name: "collect records into slice maps through medium table",
+		data: `
+			INSERT INTO countries (id, name) VALUES (1, 'Japan');
+
+			INSERT INTO cities (country_id, id, name) VALUES (1, 11, 'tokyo');
+			INSERT INTO cities (country_id, id, name) VALUES (1, 12, 'hokkaido');
+			INSERT INTO cities (country_id, id, name) VALUES (1, 13, 'okinawa');
+
+			INSERT INTO addresses (city_id, id, name) VALUES (11, 111, 'shinjuku');
+			INSERT INTO addresses (city_id, id, name) VALUES (11, 112, 'yoyogi');
+			INSERT INTO addresses (city_id, id, name) VALUES (11, 113, 'nakano');
+			INSERT INTO addresses (city_id, id, name) VALUES (12, 121, 'sapporo');
+			INSERT INTO addresses (city_id, id, name) VALUES (12, 122, 'kushiro');
+			INSERT INTO addresses (city_id, id, name) VALUES (13, 131, 'naha');
+			INSERT INTO addresses (city_id, id, name) VALUES (13, 132, 'miyako');
+
+			INSERT INTO countries (id, name) VALUES (2, 'Somewhere');
+			INSERT INTO cities (country_id, id, name) VALUES (2, 21, 'city1');
+			INSERT INTO addresses (city_id, id, name) VALUES (21, 211, 'address1');
+			INSERT INTO addresses (city_id, id, name) VALUES (21, 212, 'address2');
+		`,
+		run: func(t *testing.T, tx *goq.Tx, z *Builder) error {
+			q := z.Select(
+				z.Countries.All(),
+				z.Addresses.All(),
+			).From(
+				z.Countries,
+			).Joins(
+				z.InnerJoin(z.Cities).On(
+					z.Cities.CountryID.Eq(z.Countries.ID),
+				),
+				z.InnerJoin(z.Addresses).On(
+					z.Addresses.CityID.Eq(z.Cities.ID),
+				),
+			).OrderBy(
+				z.Countries.ID,
+				z.Cities.ID,
+				z.Addresses.ID,
+			)
+
+			var countries map[int]Country
+			var addresses map[int][]Address
+			err := tx.Query(q).Collect(
+				z.Countries.ToMap(&countries),
+				z.Addresses.ToSliceMap(&addresses).By(z.Countries.ID),
+			)
+			if err != nil {
+				return err
+			}
+
+			wantCountries := map[int]Country{
+				1: {1, "Japan", defaultUpdatedAt},
+				2: {2, "Somewhere", defaultUpdatedAt},
+			}
+			if diff := deep.Equal(countries, wantCountries); diff != nil {
+				t.Log(q.Construct())
+				return fmt.Errorf("countries diff: %s", diff)
+			}
+
+			wantAddresses := map[int][]Address{
+				1: []Address{
+					{111, "shinjuku", 11, defaultUpdatedAt},
+					{112, "yoyogi", 11, defaultUpdatedAt},
+					{113, "nakano", 11, defaultUpdatedAt},
+					{121, "sapporo", 12, defaultUpdatedAt},
+					{122, "kushiro", 12, defaultUpdatedAt},
+					{131, "naha", 13, defaultUpdatedAt},
+					{132, "miyako", 13, defaultUpdatedAt},
+				},
+				2: []Address{
+					{211, "address1", 21, defaultUpdatedAt},
+					{212, "address2", 21, defaultUpdatedAt},
+				},
+			}
+			if diff := deep.Equal(addresses, wantAddresses); diff != nil {
+				t.Log(q.Construct())
+				return fmt.Errorf("addresses diff: %s", diff)
 			}
 
 			return nil
