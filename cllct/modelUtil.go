ede5289c6092258677ccf12cbc1a06cc662f6684
ryym <ryym.64@gmail.com>
2018-02-11 14:59:05 +0900

    refac(cllct): Unify util files

--- cllct/modelUtil.go
+++
@@ -1,7 +0,0 @@
-package cllct
-
-import "github.com/ryym/goq/gql"
-
-func isSameTable(col gql.Selection, tbl tableInfo) bool {
-	return col.TableAlias == tbl.tableAlias && col.StructName == tbl.structName
-}
--- cllct/util.go
+++ cllct/util.go
@@ -3,7 +3,22 @@ package cllct
 import (
 	"fmt"
 	"reflect"
+
+	"github.com/ryym/goq/gql"
 )
+
+// For map collectors.
+func isKeyCol(col *gql.Selection, key *gql.Selection) bool {
+	if col.Alias != "" && col.Alias == key.Alias {
+		return true
+	}
+	return col.StructName != "" && col.StructName == key.StructName &&
+		col.TableAlias == key.TableAlias && col.FieldName == key.FieldName
+}
+
+func isSameTable(col gql.Selection, tbl tableInfo) bool {
+	return col.TableAlias == tbl.tableAlias && col.StructName == tbl.structName
+}
 
 func checkPtrKind(ptr interface{}, kind reflect.Kind) error {
 	tp := reflect.TypeOf(ptr)
