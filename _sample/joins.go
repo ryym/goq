9e7eb80329d853d9c22bd922a7c9f8fe1cc0be7e
ryym <ryym.64@gmail.com>
2018-02-12 10:49:28 +0900

    refac: Remove unused sample code

--- _sample/joins.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/ryym/goq/gql"
-
-func (p *Prefs) Cities(c *Cities) *gql.Join {
-	return &gql.Join{p, p.ID.Eq(c.PrefID)}
-}
--- _sample/models/models.go
+++
@@ -1,6 +0,0 @@
-package models
-
-type User struct {
-	ID   int `goq:"pk"`
-	Name string
-}
